d9a294e557a2141c0f5f4864e468352b17c41abb
Suraj Singh <yousurajsingh@gmail.com>
2024-12-29 21:40:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+    s1 = s1.toUpperCase();
+    s2 = s2.toUpperCase();
+    let length = s2.length;
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+    for(let i = 0; i <= s1.length-length; i++){
+        
+        if(s1.slice(i, i+length) === s2){
+            return i;
+        }
+    }
+    return -1;
+}
+  
+  
+  const s1 = prompt("Enter s1:");
+  const s2 = prompt("Enter s2:");
+  alert(indexOfIgnoreCase(s1, s2));
